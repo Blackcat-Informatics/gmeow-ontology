ef8b0f8737bc4d15a50239f84c980af630c227b4
Patrick_Audley <paudley@blackcatinformatics.ca>
2026-06-09 19:05:25 -0600

    fix(email): remove duplicate comments and orphaned fixture resource
    - Remove duplicated Issue #63 and Issue #22 comment blocks in
      test_mapping_compiler.py (rebase artifact).
    - Remove orphaned ex:hdrDisposition MessageHeader from coverage fixture
      (not referenced by any MessageParticipant; disposition modeled via
      gmeow:dispositionNotificationTo instead).

--- tests/test_mapping_compiler.py
+++ tests/test_mapping_compiler.py
@@ -214,13 +214,6 @@ def test_dsl_parses() -> None:
     # Issue #63 first-class annotation & PKM layer: +16 (oa:Annotation x3,
     # schema:NoteDigitalDocument, schema:Comment, as:Note, sioc:Post,
     # sioc:reply_of, sioc:has_reply, AnnotationMotivation x7).
-    # Issue #63 first-class annotation & PKM layer: +16 (oa:Annotation x3,
-    # schema:NoteDigitalDocument, schema:Comment, as:Note, sioc:Post,
-    # sioc:reply_of, sioc:has_reply, AnnotationMotivation x7).
-    # Issue #22 image super-ontology: +21 (schema.org ImageObject/about/width/
-    # height/dateCreated x5, Web Annotation selector types x4, EXIF width/height/
-    # orientation/dateTime x4, IIIF Canvas/Annotation/width/height x4,
-    # CIDOC-CRM production x1, CRMdig area x1, Wikidata region/spatial-relation x2).
     # Issue #22 image super-ontology: +21 (schema.org ImageObject/about/width/
     # height/dateCreated x5, Web Annotation selector types x4, EXIF width/height/
     # orientation/dateTime x4, IIIF Canvas/Annotation/width/height x4,
