10b2da5d5c5cf600a606c73f63f50c6d3c9735bd
Patrick_Audley <paudley@blackcatinformatics.ca>
2026-06-18 00:52:21 -0600

    regenerate(cognition): mapping artifacts and prefix fixes for alignment ledger

--- src/gmeow_tools/config.py
+++ src/gmeow_tools/config.py
@@ -440,7 +440,10 @@ PREFIXES: dict[str, str] = {
     "as": "https://www.w3.org/ns/activitystreams#",
     "mads": "http://www.loc.gov/mads/rdf/v1#",
     "esco": "http://data.europa.eu/esco/model#",
+    "esco-base": "http://data.europa.eu/esco/",
     "ceterms": "https://purl.org/ctdl/terms/",
+    "ctdlasn": "https://credreg.net/ctdlasn/terms/",
+    "onet": "https://www.onetcenter.org/",
     "nmo": "http://www.semanticdesktop.org/ontologies/2007/03/22/nmo#",
     "wot": "http://xmlns.com/wot/0.1/",
     # Verifiable Credentials / DID (the attestation module, #162)
